6976f81023830a9ee726b978bc22af8d3b2bb278
kaiyes <kaiyes.ansary@gmail.com>
2019-12-17 21:49:41 +0600

    changed to working tab nav

--- src/navigation.js
+++ src/navigation.js
@@ -1,35 +1,101 @@
-import React from "react"
-import { NavigationNativeContainer } from "@react-navigation/native"
+import React, { useState } from "react"
 import { createStackNavigator } from "@react-navigation/stack"
-//screens
-import HomeScreen from "./screens/Home"
-import DuaScreen from "./screens/Duas"
-import DeedScreen from "./screens/Deeds"
-import QuranScreen from "./screens/Quran"
-import MemorizeScreen from "./screens/Memorize"
-import LiveQuizScreen from "./screens/LiveQuiz"
-import LearnArabicScreen from "./screens/LearnArabic"
-import QuranTeacherScreen from "./screens/Qtor"
-import ZanajaScreen from "./screens/Janaza"
-import DuaDetailScreen from "./screens/DuaDetail"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Icon } from "react-native-elements"
+//Screens
+import ListScreen from "./screens/List.screen"
+import DetailScreen from "./screens/Details.screen"
+import SplashScreen from "./screens/Splash.screen"
+import ChatScreen from "./screens/Chat.screen"
+import PaymentScreen from "./screens/Payment.screen"
+import MatchScreen from "./screens/Match.screen"
+
+//components
+import IconWithBadge from "./Components/ChatIcon"
 
 const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
-export default function App() {
+function Splash() {
   return (
-    <NavigationNativeContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="home" component={HomeScreen} />
-        <Stack.Screen name="dua" component={DuaScreen} />
-        <Stack.Screen name="quran" component={QuranScreen} />
-        <Stack.Screen name="deeds" component={DeedScreen} />
-        <Stack.Screen name="memorize" component={MemorizeScreen} />
-        <Stack.Screen name="learnArabic" component={LearnArabicScreen} />
-        <Stack.Screen name="liveQuiz" component={LiveQuizScreen} />
-        <Stack.Screen name="qTor" component={QuranTeacherScreen} />
-        <Stack.Screen name="janaza" component={ZanajaScreen} />
-        <Stack.Screen name="details" component={DuaDetailScreen} />
-      </Stack.Navigator>
-    </NavigationNativeContainer>
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name="Splash" component={SplashScreen} />
+    </Stack.Navigator>
   )
+}
+
+function ListStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="List" component={ListScreen} />
+      <Stack.Screen name="Details" component={DetailScreen} />
+    </Stack.Navigator>
+  )
+}
+
+function SignUpStack() {
+  return (
+    <Stack.Navigator headerMode="none">
+      <>
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="LogIn" component={LogInScreen} />
+      </>
+    </Stack.Navigator>
+  )
+}
+
+function LoggedInStack() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size = 22 }) => {
+          let iconName
+
+          switch (route.name) {
+            case "List":
+              iconName = "home"
+              break
+            case "Chat":
+              iconName = "chat"
+              break
+            case "Payment":
+              iconName = "credit"
+              break
+            case "Match":
+              iconName = "heart"
+              break
+            default:
+          }
+
+          // You can return any component that you like here!
+          return (
+            <IconWithBadge
+              name={iconName}
+              type={"entypo"}
+              size={22}
+              color={color}
+              badgeCount={0}
+            />
+          )
+        }
+      })}
+      tabBarOptions={{
+        activeTintColor: "slateblue",
+        inactiveTintColor: "darkgrey"
+      }}
+    >
+      <Tab.Screen name="List" component={ListStack} />
+      <Tab.Screen name="Chat" component={ChatScreen} />
+      <Tab.Screen name="Payment" component={PaymentScreen} />
+      <Tab.Screen name="Match" component={MatchScreen} />
+    </Tab.Navigator>
+  )
+}
+
+export default function Navigator({ navigation }) {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [isInLimbo, setLimbo] = useState(false)
+
+  return isLoading ? Splash() : LoggedInStack()
 }
