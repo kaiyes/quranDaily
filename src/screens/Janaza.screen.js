ca4b56250b07be8b167b5e818162523117df2c04
kaiyes <kaiyes.ansary@gmail.com>
2019-12-24 20:03:22 +0600

    added marker (will change it)

--- src/screens/Janaza.screen.js
+++ src/screens/Janaza.screen.js
@@ -7,39 +7,39 @@ import {
   Text,
   StatusBar
 } from "react-native"
-import MapView from "react-native-maps"
+import MapView, { Marker } from "react-native-maps"
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen"
 
 export default function Janaza() {
-  // const [region, setRegion] = useState({
-  //   latitude: 37.78825,
-  //   longitude: -122.4324,
-  //   latitudeDelta: 0.0922,
-  //   longitudeDelta: 0.0421
-  // })
+  const [region, setRegion] = useState({
+    latitude: 23.8211,
+    longitude: 90.3704,
+    latitudeDelta: 1,
+    longitudeDelta: 1
+  })
   return (
     <MapView
-      style={{ height: hp("100%"), width: wp("100%") }}
+      style={styles.map}
       initialRegion={{
-        latitude: 23.7916,
-        longitude: 90.4152,
+        latitude: 23.8211,
+        longitude: 90.3704,
         latitudeDelta: 1,
         longitudeDelta: 1
       }}
       showsUserLocation={true}
-    />
+      minZoomLevel={10}
+    >
+      <Marker coordinate={region} />
+    </MapView>
   )
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: "white"
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600"
+  map: {
+    height: hp("100%"),
+    width: wp("100%")
   }
 })
