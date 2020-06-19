ccf1a452e9906353341b5170e1eb5b5b2aeb502a
kaiyes <kaiyes.ansary@gmail.com>
2020-06-19 12:15:39 +0600

    added quran index page

--- src/screens/Quran.screen.js
+++ src/screens/Quran.screen.js
@@ -5,14 +5,15 @@ import {
 	ScrollView,
 	View,
 	Text,
-	FlatList
+	FlatList,
+	TouchableOpacity
 } from 'react-native'
 import {
 	widthPercentageToDP as wp,
 	heightPercentageToDP as hp
 } from 'react-native-responsive-screen'
 
-import Sura from '../utility/sura.js'
+import Suras from '../utility/suras.js'
 import '../assets/fonts/me_quran.ttf'
 
 export default function Quran() {
@@ -25,13 +26,35 @@ export default function Quran() {
 		)
 	}
 
+	function suraBlock(item) {
+		return (
+			<TouchableOpacity
+				style={
+					item.number % 2 === 0
+						? [styles.suraBlock, { backgroundColor: 'mediumaquamarine' }]
+						: [styles.suraBlock, { backgroundColor: 'honeydew' }]
+				}>
+				<Text style={styles.number}>{item.number}</Text>
+				<View style={styles.nameBlock}>
+					<View style={styles.nameRow}>
+						<Text style={styles.name}>{item.transliteration_en}</Text>
+						<Text style={styles.italicSubtitle}>{item.transliteration_en}</Text>
+					</View>
+					<Text style={styles.subTitle}>
+						{item.revelation_type} - {item.total_verses} verses
+					</Text>
+				</View>
+			</TouchableOpacity>
+		)
+	}
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<FlatList
-				data={Sura.verses}
+				data={Suras}
 				keyExtractor={item => item.number}
 				contentContainerStyle={styles.flatList}
-				renderItem={({ item }) => ayah(item)}
+				renderItem={({ item }) => suraBlock(item)}
 			/>
 		</SafeAreaView>
 	)
@@ -40,12 +63,16 @@ export default function Quran() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'white'
+		backgroundColor: 'honeydew'
 	},
 	ayah: {
-		fontSize: 24,
+		fontSize: 32,
 		fontFamily: 'me_quran',
 		marginBottom: hp('1%')
+	},
+	name: {
+		fontSize: 17,
+		fontFamily: 'Menlo'
 	},
 	ayahBlock: {
 		width: wp('100%'),
@@ -56,9 +83,34 @@ const styles = StyleSheet.create({
 		borderBottomWidth: 1,
 		borderBottomColor: 'silver'
 	},
+	suraBlock: {
+		width: wp('100%'),
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingHorizontal: wp('5%'),
+		paddingVertical: hp('3%')
+	},
 	translation: {
 		fontSize: 17,
 		fontFamily: 'Menlo',
 		marginBottom: hp('1%')
+	},
+	subTitle: {
+		fontSize: 14,
+		fontWeight: '200',
+		marginTop: hp('.5%')
+	},
+	number: {
+		fontSize: 24,
+		marginRight: wp('4%')
+	},
+	nameRow: {
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	italicSubtitle: {
+		fontWeight: '200',
+		fontStyle: 'italic',
+		marginLeft: wp('2%')
 	}
 })
