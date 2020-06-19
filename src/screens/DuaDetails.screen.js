6a25886acd04e57c2da9db16485d701be29b76a6
kaiyes <kaiyes.ansary@gmail.com>
2020-06-19 18:47:35 +0600

    fixed dua detail double back button

--- src/screens/DuaDetails.screen.js
+++ src/screens/DuaDetails.screen.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 import {
 	StyleSheet,
 	View,
@@ -7,30 +7,20 @@ import {
 	TouchableOpacity,
 	ScrollView,
 	SafeAreaView
-} from "react-native"
+} from 'react-native'
 import {
 	widthPercentageToDP as wp,
 	heightPercentageToDP as hp
-} from "react-native-responsive-screen"
-import { Icon } from "react-native-elements"
+} from 'react-native-responsive-screen'
+import { Icon } from 'react-native-elements'
 //utility
-import "../assets/fonts/me_quran.ttf"
+import '../assets/fonts/me_quran.ttf'
 
 export default function DuaDetail({ route, navigation }) {
 	const { pageTitle, duas } = route.params
 
 	return (
 		<SafeAreaView style={styles.rootView}>
-			<View style={styles.backNav}>
-				<TouchableOpacity
-					onPress={() => {
-						navigation.goBack()
-					}}
-				>
-					<Icon name="arrow-bold-left" type="entypo" color="dimgray" />
-				</TouchableOpacity>
-			</View>
-
 			<ScrollView style={styles.backgroundScrollView}>
 				<Text style={styles.title}>{pageTitle}</Text>
 
@@ -73,76 +63,78 @@ export default function DuaDetail({ route, navigation }) {
 const styles = StyleSheet.create({
 	rootView: {
 		flex: 1,
-		backgroundColor: "honeydew",
-		paddingTop: hp("3%"),
-		paddingBottom: hp("5%")
+		backgroundColor: 'honeydew',
+		paddingTop: hp('4%'),
+		paddingBottom: hp('5%')
 	},
 	backgroundScrollView: {
-		backgroundColor: "honeydew",
-		paddingHorizontal: wp("5%")
+		backgroundColor: 'honeydew',
+		paddingHorizontal: wp('5%'),
+
+		paddingTop: hp('2%')
 	},
 	secondContainer: {
-		width: wp("90%")
+		width: wp('90%')
 	},
 	title: {
 		fontSize: 22,
-		fontWeight: "bold",
-		color: "darkolivegreen",
-		textAlign: "center",
-		textDecorationLine: "underline"
+		fontWeight: 'bold',
+		color: 'darkolivegreen',
+		textAlign: 'center',
+		textDecorationLine: 'underline'
 	},
 	dua: {
-		marginTop: hp("2%"),
+		marginTop: hp('2%'),
 		fontSize: 32,
-		fontWeight: "300",
-		textAlign: "right",
-		fontFamily: "me_quran",
-		color: "darkolivegreen"
+		fontWeight: '300',
+		textAlign: 'right',
+		fontFamily: 'me_quran',
+		color: 'darkolivegreen'
 	},
 	spelling: {
-		marginTop: hp("3%"),
+		marginTop: hp('3%'),
 		fontSize: 18,
-		fontWeight: "400",
-		fontFamily: "Menlo",
-		color: "darkolivegreen"
+		fontWeight: '400',
+		fontFamily: 'Menlo',
+		color: 'darkolivegreen'
 	},
 	meaning: {
-		marginTop: hp("3%"),
+		marginTop: hp('3%'),
 		fontSize: 18,
-		fontWeight: "400",
-		fontFamily: "Menlo",
-		color: "darkolivegreen"
+		fontWeight: '400',
+		fontFamily: 'Menlo',
+		color: 'darkolivegreen'
 	},
 	source: {
-		marginTop: hp("2%"),
+		marginTop: hp('2%'),
 		fontSize: 16,
-		fontWeight: "200",
-		fontFamily: "Menlo",
-		color: "darkolivegreen"
+		fontWeight: '200',
+		fontFamily: 'Menlo',
+		color: 'darkolivegreen'
 	},
 	playButton: {
-		position: "absolute",
-		right: wp("5%"),
-		bottom: hp("3.5%"),
-		width: wp("27%"),
-		height: hp("20%")
+		position: 'absolute',
+		right: wp('5%'),
+		bottom: hp('3.5%'),
+		width: wp('27%'),
+		height: hp('20%')
 	},
 	preSpell: {
 		fontSize: 16,
-		fontWeight: "bold",
-		fontFamily: "Menlo",
-		color: "darkgreen",
-		textDecorationLine: "underline",
-		marginRight: wp("10%")
+		fontWeight: 'bold',
+		fontFamily: 'Menlo',
+		color: 'darkgreen',
+		textDecorationLine: 'underline',
+		marginRight: wp('10%')
 	},
 	backNav: {
-		width: wp("100%"),
-		height: hp("5%"),
-		alignItems: "flex-start",
-		justifyContent: "center",
-		paddingLeft: wp("3.5%"),
-		marginBottom: hp("2%"),
-		borderBottomWidth: hp(".2%"),
-		borderBottomColor: "whitesmoke"
+		width: wp('100%'),
+		height: hp('5%'),
+		alignItems: 'flex-start',
+		justifyContent: 'center',
+		paddingLeft: wp('3.5%'),
+		marginBottom: hp('2%'),
+		borderBottomWidth: hp('.2%'),
+		borderBottomColor: 'whitesmoke'
 	}
 })
