e5d46bf3c68c09e1bd61a22c24926f9f8f43e0d7
kaiyes <kaiyes.ansary@gmail.com>
2020-07-21 14:29:07 +0600

    took out pop up menu

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ import 'react-native-gesture-handler'
 import React, { Component } from 'react'
 import { AppRegistry } from 'react-native'
 import { name as appName } from './app.json'
-import { MenuProvider } from 'react-native-popup-menu'
 import { NavigationNativeContainer } from '@react-navigation/native'
 
 import Navigator from './src/navigation'
 
 export default class App extends Component {
-	render() {
-		return (
-			<NavigationNativeContainer>
-				<MenuProvider>
-					<Navigator />
-				</MenuProvider>
-			</NavigationNativeContainer>
-		)
-	}
+  render() {
+    return (
+      <NavigationNativeContainer>
+        <Navigator />
+      </NavigationNativeContainer>
+    )
+  }
 }
 
 AppRegistry.registerComponent(appName, () => App)
