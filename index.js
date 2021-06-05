5771f7c9884c3f22080153ad086e12706931e0be
kaiyes <kaiyes.ansary@gmail.com>
2021-06-05 17:10:16 +0600

    new app

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-import 'react-native-gesture-handler'
-import React, { useEffect } from 'react'
-import { AppRegistry } from 'react-native'
-import { name as appName } from './app.json'
-import { NavigationNativeContainer } from '@react-navigation/native'
-import { NotifierWrapper } from 'react-native-notifier'
-import SplashScreen from 'react-native-splash-screen'
+/**
+ * @format
+ */
 
-import Navigator from './src/navigation'
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-export default function App() {
-	useEffect(() => SplashScreen.hide(), [])
-
-	return (
-		<NavigationNativeContainer>
-			<NotifierWrapper>
-				<Navigator />
-			</NotifierWrapper>
-		</NavigationNativeContainer>
-	)
-}
-
-console.disableYellowBox = true
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => App);
